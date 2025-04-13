c886048126e0585cb5ec9250d70830bbbe20d031
SeoDeveloper01 <91273094+SeoDeveloper01@users.noreply.github.com>
2025-04-13 03:18:28 +0300

    feat: improve task-manager, add method for pretty task-list output

--- task-tracker/src/task/task-manager.ts
+++ task-tracker/src/task/task-manager.ts
@@ -34,14 +34,31 @@ export default class TaskManager {
 		return delete this.storage.items[taskID];
 	}
 
-	public getTaskList(status?: Task['status']): Task[] {
-		const tasks = new Array<Task>();
+	public getTaskList(status?: Task['status']): Storage['items'] {
+		const tasks: Storage['items'] = Object.create(null);
+		const timeFormat = Intl.DateTimeFormat(undefined, {
+			timeStyle: 'short',
+			dateStyle: 'short',
+			hour12: false
+		});
 
 		for (const taskID in this.storage.items) {
 			const task = this.storage.items[taskID]!;
-			if (task.status === (status ?? task.status)) tasks.push(task);
+
+			if (task.status === (status ?? task.status)) {
+				tasks[taskID] = Object.assign(Object.create(null), task, {
+					createdAt: timeFormat.format(task.createdAt),
+					updatedAt: timeFormat.format(task.updatedAt)
+				});
+			}
 		}
 
 		return tasks;
+	}
+
+	public logTaskList(items: Storage['items']): void {
+		const props = new Array<keyof Task>('description', 'status', 'createdAt', 'updatedAt');
+
+		console.table(items, props);
 	}
 }
